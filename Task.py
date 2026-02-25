4a243aeab9bba7f0a1c5df65a9a2dc3b8207953e
mina12323 <148848731+mina12323@users.noreply.github.com>
2026-02-25 09:37:09 +0200

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,46 @@
+# 1️⃣ Password Function
+def check_password():
+    while input("Enter password: ") != "1234":
+        print("Wrong password")
+    print("Access granted")
+
+
+# 2️⃣ Factorial Function
+def factorial():
+    n = int(input("Enter number: "))
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    print("Factorial =", fact)
+
+
+# 3️⃣ Guessing Game Function
+def guessing_game():
+    while int(input("Guess the number: ")) != 7:
+        print("Try again")
+    print("Correct!")
+
+
+# 4️⃣ Prime Check Function
+def check_prime():
+    n = int(input("Enter number: "))
+    prime = True
+
+    if n <= 1:
+        prime = False
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                prime = False
+
+    if prime:
+        print("Prime number")
+    else:
+        print("Not prime")
+
+
+# Calling the functions
+check_password()
+factorial()
+guessing_game()
+check_prime()
